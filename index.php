7d0495cdd111520ba9fc5bbf748b54a7b2110ee7
Dhiraj <jireldhiraj123@gmail.com>
2024-02-08 09:55:49 +0545

    include header to index

--- index.php
+++ index.php
@@ -8,20 +8,7 @@
 </head>
 <body>
     <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-3">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-              <img class="bi me-2" src="image/me-circle.png" alt="Dhiraj Jirel" width="50" height="50">
-              <span class="fs-4">Dhiraj Jirel</span>
-            </a>
-      
-            <ul class="nav nav-pills">
-              <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-              <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-              <li class="nav-item"><a href="#" class="nav-link">Skill</a></li>
-              <li class="nav-item"><a href="#" class="nav-link">Project</a></li>
-              <li class="nav-item"><a href="#" class="nav-link">Gallery</a></li>
-            </ul>
-        </header>
+        <?php include 'common/header.php'; ?>
     </div>
 
 </body>
